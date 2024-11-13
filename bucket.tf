5faebd93ea1a88c415e9ec2949c1c3834fdcecc3
Mikolo01 <mcolowoyeye2010@gmail.com>
2024-11-13 10:10:55 +0100

    update the tf files and readme file

--- bucket.tf
+++ bucket.tf
@@ -1,6 +1,13 @@
-resource "google_storage_bucket" "gcp-bucket" {
-  name     = "mikolo01-bucket"
-  location = "US" 
+resource "google_storage_bucket" "mikolo-bucket1" {
+  name     = "mikolo-bucket1"
+  location = "us" 
+  storage_class = "STANDARD"
+  uniform_bucket_level_access = true
+}
+
+resource "google_storage_bucket" "mikolo-bucket2" {
+  name     = "mikolo-bucket2"
+  location = "eu" 
   storage_class = "STANDARD"
   uniform_bucket_level_access = true
 }
--- vm.tf
+++ vm.tf
@@ -1,18 +1,40 @@
-resource "google_compute_instance" "test-vm" {
-  name         = "my-vm"
-  machine_type = "e2-micro"          
+resource "google_compute_instance" "mikolo-instance1" {
+  name         = "my-vm1"
+  machine_type = "e2-medium"          
   zone         = "us-central1-a"
 
   boot_disk {
     initialize_params {
-      image = "ubuntu-os-cloud/ubuntu-2004-lts"
+      image = "ubuntu-os-cloud/ubuntu-2204-lts"
     }
   }
 
   network_interface {
-    subnetwork = "my-subnet"
+    network    = google_compute_network.vpc_network.id
+    subnetwork = "my-subnet1"
 
-    access_config {  
+    access_config {}
+  }
+
+  tags = ["http-allowed", "https-allowed"]
+}
+
+resource "google_compute_instance" "mikolo-instance2" {
+  name         = "my-vm2"
+  machine_type = "n2-standard-2"
+  zone         = "europe-west10-c"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-12"
     }
   }
+
+  network_interface {
+    network    = google_compute_network.vpc_network.id
+    subnetwork = "my-subnet2"
+    access_config {}
+  }
+
+  tags = ["http-allowed", "https-allowed"]
 }
